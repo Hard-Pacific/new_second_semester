0a3cd704e8539d7bbae201f120ad81855e7db910
HardPacific <93593742+Hard-Pacific@users.noreply.github.com>
2024-09-06 18:55:08 +0500

    Update Huffman

--- Huffman/Huffman/Program.cs
+++ Huffman/Huffman/Program.cs
@@ -1,23 +1,29 @@
 ﻿using HuffmanCompressor;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine(
+                "Invalid number of arguments. Use:" +
+                "HuffmanCompressor.exe [filename] [--compress | --uncompress | -c | -u]");
+        }
+        string filename = args[0];
+        string operation = args[1];
 
-if (args.Length != 2)
-{
-    Console.WriteLine(
-        "Неверное количество аргументов. Используйте: " +
-        "HuffmanCompressor.exe [имя_файла] [--compress | --uncompress | -c | -u]");
-}
-string filename = args[0];
-string operation = args[1];
+        if (operation == "--compress" || operation == "-c")
+        {
+            Huffman.Compress(filename);
+        }
+        else if (operation == "--uncompress" || operation == "-u")
+        {
+            Huffman.Uncompress(filename);
+        }
+        else
+        {
+            Console.WriteLine("Invalid operation key. Use: --compress | --uncompress | -c | -u");
+        }
 
-if (operation == "--compress" || operation == "-c")
-{
-    Huffman.Compress(filename);
-}
-else if (operation == "--uncompress" || operation == "-u")
-{
-    Huffman.Uncompress(filename);
-}
-else
-{
-    Console.WriteLine("Неверный ключ операции. Используйте: --compress | --uncompress | -c | -u");
+    }
 }
